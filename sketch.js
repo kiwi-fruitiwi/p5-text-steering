36e7da5790f3d44f120a573538cb85480bd67bc1
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-12 11:11:56 -0700

    Add textToPoints!

--- sketch.js
+++ sketch.js
@@ -9,13 +9,23 @@ function setup() {
   colorMode(HSB, 360, 100, 100, 100);
 
   textFont(font);
-  textSize(128);
-  fill(0, 0, 100);
+  textSize(192);
+  fill(0, 0, 20);
   noStroke();
+  background(0, 0, 30);
+
+  text('train', 100, 200);
+  let points = font.textToPoints('train', 100, 200);
+
+  for (let i=0; i<points.length; i++) {
+    let pt = points[i];
+    stroke(80, 100, 70, 100);
+    strokeWeight(4);
+    point(pt.x, pt.y);
+  }
 }
 
 function draw() {
-  background(0, 0, 30);
-  ellipse(mouseX, mouseY, 50, 50);
-  text('train', 100, 200);
+  // background(0, 0, 30);
+  // ellipse(mouseX, mouseY, 50, 50);
 }
